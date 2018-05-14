886e9cb983c46498003878afe965d55caa762025
Gav Wood <github@gavwood.com>
2018-05-14 17:16:02 +0200

    Add 128 bit types.

--- src/lib.rs
+++ src/lib.rs
@@ -84,7 +84,7 @@ macro_rules! impl_isqrt {
 }
 
 
-impl_isqrt!(usize, u64, u32, u16, u8, isize, i64, i32, i16, i8);
+impl_isqrt!(usize, u128, u64, u32, u16, u8, isize, i128, i64, i32, i16, i8);
 
 
 #[cfg(test)]
